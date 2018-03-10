6109f347236e42f12db54c324ddb1a1f012d83b8
Tomáš Valenta <semanticer@gmail.com>
2018-03-10 16:10:11 +0100

    RecyclerView display of multiple currencies

---
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/CryptoAdapter.kt
@@ -0,0 +1,56 @@
+package cz.geekyedu.geekyedu.presentation
+
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import cz.geekyedu.geekyedu.R
+import cz.geekyedu.geekyedu.data.model.CryptoCurrency
+import cz.geekyedu.geekyedu.presentation.utils.CryptoIconHelper
+import cz.geekyedu.geekyedu.presentation.utils.GlideApp
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_crypto.*
+
+class CryptoAdapter : ListAdapter<CryptoCurrency, CryptoAdapter.ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_crypto, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindTo(getItem(position))
+    }
+
+
+    class ViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bindTo(model: CryptoCurrency) {
+            val nameText = "${model.symbol} | ${model.name}"
+            val priceText = "$ ${model.priceUsd}"
+            nameView.text = nameText
+            priceView.text = priceText
+            setPercentValue(model.percentChange24h, dayPercent)
+            setPercentValue(model.percentChange7d, weekPercent)
+            val iconUrl = CryptoIconHelper.getIconUrl(model.id)
+            GlideApp
+                    .with(containerView)
+                    .load(iconUrl)
+                    .into(iconView)
+        }
+
+        private fun setPercentValue(percent: Double, valueView: TextView) {
+            val percentText = "$percent%"
+            valueView.text = percentText
+            val textColor = if (percent >= 0 ) R.color.plus else R.color.minus
+            valueView.setTextColor(containerView.context.resources.getColor(textColor, null))
+        }
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<CryptoCurrency>() {
+        override fun areItemsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = oldItem.id == newItem.id
+        override fun areContentsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = oldItem == newItem
+    }
+}
--- app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
@@ -2,62 +2,58 @@ package cz.geekyedu.geekyedu.presentation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
+import androidx.view.isVisible
 import cz.geekyedu.geekyedu.R
 import cz.geekyedu.geekyedu.data.model.CryptoCurrency
 import cz.geekyedu.geekyedu.data.remote.CryptoProvider
-import cz.geekyedu.geekyedu.presentation.utils.CryptoIconHelper
-import cz.geekyedu.geekyedu.presentation.utils.GlideApp
 import cz.geekyedu.geekyedu.presentation.utils.setEnqueue
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cryptoAdapter: CryptoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadData()
+
+        cryptoAdapter = CryptoAdapter()
+        val viewManager = LinearLayoutManager(this)
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = cryptoAdapter
+        }
+        loadData(10)
     }
 
-    private fun loadData() {
-        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoProvider.cryptoService.cryptoList(10)
+    private fun loadData(limit: Int) {
+        progressView.isVisible = true
+        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoProvider.cryptoService.cryptoList(limit)
         cryptoListRequest.setEnqueue(
                 onResponse = { call, response ->
                     if (response.isSuccessful) {
-                        val cryptoCurrency = response.body()!![0]
-                        showModel(cryptoCurrency)
+                        val cryptoCurrencyList = response.body()!!
+                        cryptoAdapter.submitList(cryptoCurrencyList)
                     } else {
                         showRemoteError()
                     }
+                    progressView.isVisible = false
                 },
-                onFailure = { call, t -> showRemoteError() }
+                onFailure = { call, t ->
+                    showRemoteError()
+                    progressView.isVisible = false
+                }
         )
     }
 
-    private fun showModel(model: CryptoCurrency) {
-        val nameText = "${model.symbol} | ${model.name}"
-        val priceText = "$ ${model.priceUsd}"
-        nameView.text = nameText
-        priceView.text = priceText
-        setPercentValue(model.percentChange24h, dayPercent)
-        setPercentValue(model.percentChange7d, weekPercent)
-        val iconUrl = CryptoIconHelper.getIconUrl(model.id)
-        GlideApp
-            .with(this)
-            .load(iconUrl)
-            .into(iconView)
-    }
-
-    private fun setPercentValue(percent: Double, valueView: TextView) {
-        val percentText = "$percent%"
-        valueView.text = percentText
-        val textColor = if (percent >= 0 ) R.color.plus else R.color.minus
-        valueView.setTextColor(getColor(textColor))
-    }
 
     private fun showRemoteError() {
         Toast.makeText(this, R.string.network_error, Toast.LENGTH_LONG).show()
+
     }
 }
