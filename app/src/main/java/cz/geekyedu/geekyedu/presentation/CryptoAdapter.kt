75fe19f8c3def3a6fbb5e4b7d8ab71d857e8740f
Tomáš Valenta <semanticer@gmail.com>
2018-03-23 17:24:27 +0100

    Remove almost all adapter stuff and add TODO's

--- app/src/main/java/cz/geekyedu/geekyedu/presentation/CryptoAdapter.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/CryptoAdapter.kt
@@ -3,54 +3,28 @@ package cz.geekyedu.geekyedu.presentation
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import cz.geekyedu.geekyedu.R
 import cz.geekyedu.geekyedu.data.model.CryptoCurrency
-import cz.geekyedu.geekyedu.presentation.utils.CryptoIconHelper
-import cz.geekyedu.geekyedu.presentation.utils.GlideApp
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_crypto.*
 
 class CryptoAdapter : ListAdapter<CryptoCurrency, CryptoAdapter.ViewHolder>(DiffCallback) {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_crypto, parent, false)
-        return ViewHolder(itemView)
+        // TODO create new layout for one row, inflate it here and create new view holder with it
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindTo(getItem(position))
+        // TODO bind current item to holder
     }
 
     class ViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
-        fun bindTo(model: CryptoCurrency) {
-            val nameText = "${model.symbol} | ${model.name}"
-            val priceText = "$ ${model.priceUsd}"
-            nameView.text = nameText
-            priceView.text = priceText
-            setPercentValue(model.percentChange24h, dayPercent)
-            setPercentValue(model.percentChange7d, weekPercent)
-            val iconUrl = CryptoIconHelper.getIconUrl(model.id)
-            GlideApp
-                    .with(containerView)
-                    .load(iconUrl)
-                    .into(iconView)
-        }
-
-        private fun setPercentValue(percent: Double, valueView: TextView) {
-            val percentText = "$percent%"
-            valueView.text = percentText
-            val textColor = if (percent >= 0 ) R.color.plus else R.color.minus
-            valueView.setTextColor(containerView.context.resources.getColor(textColor, null))
-        }
+        // TODO add way to bind CryptoCurrency to this view holder
     }
 
     object DiffCallback : DiffUtil.ItemCallback<CryptoCurrency>() {
-        override fun areItemsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = oldItem.id == newItem.id
-        override fun areContentsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = oldItem == newItem
+        override fun areItemsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = TODO("How to check that these two Cryptos are the same")
+        override fun areContentsTheSame(oldItem: CryptoCurrency, newItem: CryptoCurrency) = TODO("How to check that their contents are the same")
     }
 }
--- app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
@@ -2,11 +2,13 @@ package cz.geekyedu.geekyedu.presentation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import android.widget.TextView
 import androidx.view.isVisible
 import cz.geekyedu.geekyedu.R
 import cz.geekyedu.geekyedu.data.model.CryptoCurrency
 import cz.geekyedu.geekyedu.data.remote.CryptoService
+import cz.geekyedu.geekyedu.presentation.utils.CryptoIconHelper
+import cz.geekyedu.geekyedu.presentation.utils.GlideApp
 import cz.geekyedu.geekyedu.presentation.utils.setEnqueue
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -20,30 +22,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setup recyclerView
-        cryptoAdapter = CryptoAdapter()
-        val viewManager = LinearLayoutManager(this)
-        recyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = cryptoAdapter
-        }
+        // TODO crate and setup all the cryptoAdapter & recyclerView stuff
 
         loadCryptoList()
     }
 
     private fun loadCryptoList() {
         progressView.isVisible = true
-        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoService.instance.cryptoList(10)
+        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoService.instance.cryptoList(1)
         cryptoListRequest.setEnqueue(
                 onResponse = { call, response ->
                     if (response.isSuccessful) {
-                        cryptoAdapter.submitList(response.body())
+                        // TODO update cryptoAdapter with the new data
                     }
                     progressView.isVisible = false
                 },
                 onFailure = { call, t -> progressView.isVisible = false }
         )
+    }
+
+    // TODO all this binding logic should end up somewhere else
+    private fun bindTo(model: CryptoCurrency) {
+        val nameText = "${model.symbol} | ${model.name}"
+        val priceText = "$ ${model.priceUsd}"
+        nameView.text = nameText
+        priceView.text = priceText
+        setPercentValue(model.percentChange24h, dayPercent)
+        setPercentValue(model.percentChange7d, weekPercent)
+        val iconUrl = CryptoIconHelper.getIconUrl(model.id)
+        GlideApp
+                .with(this)
+                .load(iconUrl)
+                .into(iconView)
+    }
+
+    private fun setPercentValue(percent: Double, valueView: TextView) {
+        val percentText = "$percent%"
+        valueView.text = percentText
+        val textColor = if (percent >= 0 ) R.color.plus else R.color.minus
+        valueView.setTextColor(resources.getColor(textColor, null))
     }
 }
 
