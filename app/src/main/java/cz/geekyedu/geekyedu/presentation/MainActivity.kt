c5f338b411dad2633a306f138d344701226ec570
Tomáš Valenta <semanticer@gmail.com>
2018-03-23 16:51:50 +0100

    Remove ViewModel logic extraction and document what should be implemented

--- app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
@@ -1,25 +1,29 @@
 package cz.geekyedu.geekyedu.presentation
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import androidx.view.isVisible
 import cz.geekyedu.geekyedu.R
+import cz.geekyedu.geekyedu.data.model.CryptoCurrency
+import cz.geekyedu.geekyedu.data.remote.CryptoService
+import cz.geekyedu.geekyedu.presentation.utils.setEnqueue
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var cryptoAdapter: CryptoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // setup recyclerView
-        val cryptoAdapter = CryptoAdapter()
+        cryptoAdapter = CryptoAdapter()
         val viewManager = LinearLayoutManager(this)
         recyclerView.apply {
             setHasFixedSize(true)
@@ -29,14 +33,24 @@ class MainActivity : AppCompatActivity() {
 
         // setup view model observers and listeners
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.apply {
-            cryptoList.observe(this@MainActivity, Observer {   list ->
-                list?.let { cryptoAdapter.submitList(it) }
-            })
-            loadingVisibility.observe(this@MainActivity, Observer { isVisible ->
-                progressView.isVisible = isVisible ?: false
-            })
-        }
+        // TODO observe liveData of progressView visibility and liveData of List<CryptoCurrency>
+
+        loadCryptoList() // TODO remove this
+    }
+
+    // TODO this should be in viewModel now and distribute new data via LiveData stream to this activity
+    private fun loadCryptoList() {
+        progressView.isVisible = true
+        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoService.instance.cryptoList(10)
+        cryptoListRequest.setEnqueue(
+                onResponse = { call, response ->
+                    if (response.isSuccessful) {
+                        cryptoAdapter.submitList(response.body())
+                    }
+                    progressView.isVisible = false
+                },
+                onFailure = { call, t -> progressView.isVisible = false }
+        )
     }
 }
 
--- app/src/main/java/cz/geekyedu/geekyedu/presentation/MainViewModel.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/MainViewModel.kt
@@ -4,32 +4,11 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import cz.geekyedu.geekyedu.data.model.CryptoCurrency
-import cz.geekyedu.geekyedu.data.remote.CryptoService
-import cz.geekyedu.geekyedu.presentation.utils.setEnqueue
-import retrofit2.Call
 
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     val loadingVisibility = MutableLiveData<Boolean>()
-    val cryptoList by lazy { loadCryptoList() }
+    val cryptoList: MutableLiveData<List<CryptoCurrency>> by lazy { /* Return liveData with List<CryptoCurrency>  */ }
 
-    private fun loadCryptoList(): MutableLiveData<List<CryptoCurrency>> {
-        val liveData = MutableLiveData<List<CryptoCurrency>>()
-        showLoading()
-        val cryptoListRequest: Call<List<CryptoCurrency>> = CryptoService.instance.cryptoList(10)
-        cryptoListRequest.setEnqueue(
-                onResponse = { call, response ->
-                    if (response.isSuccessful) {
-                        liveData.value = response.body()
-                    }
-                    hideLoading()
-                },
-                onFailure = { call, t -> hideLoading() }
-        )
-        return liveData
-    }
-
-    private fun showLoading() { loadingVisibility.value = true }
-    private fun hideLoading() { loadingVisibility.value = false }
 }
