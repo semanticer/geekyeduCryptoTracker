406d480d1dbce426bc23728697e87f5b3bd067d1
Tomáš Valenta <semanticer@gmail.com>
2018-04-02 15:03:12 +0200

    Remove all remote related things

--- app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/MainActivity.kt
@@ -16,13 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadFirstCrypto()
-    }
 
-    private fun loadFirstCrypto() {
-        progressView.isVisible = true
-        // TODO use CryptoService instance to load crypto currencies and send the first one to bindTo method. Use helper extension method setEnqueue to set handler for Call
-        val cryptoListRequest: Call<List<CryptoCurrency>> = TODO("Get request")
+        val singleTestCrypto = CryptoCurrency("bitcoin", "Bitcoin", "BTC", 7032.50, 13.2, 133.5)
+        bindTo(singleTestCrypto)
+        progressView.isVisible = false
     }
 
     private fun bindTo(model: CryptoCurrency) {
--- app/src/main/java/cz/geekyedu/geekyedu/presentation/utils/RequestUtils.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/presentation/utils/RequestUtils.kt
@@ -1,23 +1,23 @@
 package cz.geekyedu.geekyedu.presentation.utils
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
 
-fun <T> Call<T>.setEnqueue(
-        onResponse : (call: Call<T>, response: Response<T>) -> Unit,
-        onFailure : (call: Call<T>, t: Throwable) -> Unit
-){
-    enqueue(object : Callback<T> {
-        override fun onFailure(call: Call<T>, t: Throwable) {
-            onFailure(call, t)
-        }
-
-        override fun onResponse(
-                call: Call<T>,
-                response: Response<T>
-        ) {
-            onResponse(call, response)
-        }
-    })
-}
+//fun <T> Call<T>.setEnqueue(
+//        onResponse : (call: Call<T>, response: Response<T>) -> Unit,
+//        onFailure : (call: Call<T>, t: Throwable) -> Unit
+//){
+//    enqueue(object : Callback<T> {
+//        override fun onFailure(call: Call<T>, t: Throwable) {
+//            onFailure(call, t)
+//        }
+//
+//        override fun onResponse(
+//                call: Call<T>,
+//                response: Response<T>
+//        ) {
+//            onResponse(call, response)
+//        }
+//    })
+//}
