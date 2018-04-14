cc7bb8d8f55c704ff2fe42475b660d73209800e4
Tomáš Valenta <semanticer@gmail.com>
2018-04-14 23:18:49 +0200

    Simplify room database

--- app/src/main/java/cz/geekyedu/geekyedu/data/db/CryptoDataBase.kt
+++ app/src/main/java/cz/geekyedu/geekyedu/data/db/CryptoDataBase.kt
@@ -1,31 +1,9 @@
 package cz.geekyedu.geekyedu.data.db
 
 import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
-import android.content.Context
 
 @Database(entities = [(CryptoCurrencyAmount::class)], version = 1)
 abstract class CryptoDataBase : RoomDatabase() {
-
     abstract fun cryptoCurrencyDao(): CryptoCurrencyDao
-
-    companion object {
-        private var INSTANCE: CryptoDataBase? = null
-
-        fun getInstance(context: Context): CryptoDataBase? {
-            if (INSTANCE == null) {
-                synchronized(CryptoDataBase::class) {
-                    INSTANCE = Room.databaseBuilder(context.applicationContext,
-                            CryptoDataBase::class.java, "cryptodb.db")
-                            .build()
-                }
-            }
-            return INSTANCE
-        }
-
-        fun destroyInstance() {
-            INSTANCE = null
-        }
-    }
 }
